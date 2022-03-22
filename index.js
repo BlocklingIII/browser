10ab1a95de264264d6312491452b439cba606a83
BlocklingIII <67322829+BlocklingIII@users.noreply.github.com>
2022-03-22 15:22:59 +0100

    iframe

---
+++ index.js
@@ -0,0 +1,16 @@
+let button = document.getElementById('search');
+
+button.addEventListener('click', search());
+
+function search() 
+{
+    let research = document.getElementById('research').value;
+    if (research = "google.com")
+    {
+        let iframe = document.createElement('iframe');
+        iframe.src = "google.com";
+        iframe.width = "400";
+        iframe.height = "400";
+        iframe.frameBorder = "0";
+    }
+}
