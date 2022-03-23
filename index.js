385a5bb1f63a08a31f7843de7a0278c53873873a
BlocklingIII <67322829+BlocklingIII@users.noreply.github.com>
2022-03-23 08:25:50 +0100

    adding value display

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let button = document.getElementById('search');
 let canvas = document.getElementById('canvas');
+let search_bar = document.getElementById('search-bar');
 
 button.onclick = function search() 
 {
     let research = document.getElementById('resarch').value;
+    search_bar.innerHTML += "<p>" + research + "</p>";
     if (research = "google.com")
     {
         let iframe = document.createElement('iframe');
@@ -11,6 +13,8 @@ button.onclick = function search()
         iframe.width = "400";
         iframe.height = "400";
         iframe.frameBorder = "0";
+        iframe.style.border = "none";
+        iframe.style.background ="black";
         canvas.appendChild(iframe);
     }
 }
